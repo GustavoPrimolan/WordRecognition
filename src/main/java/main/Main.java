0e72b2d67d0c9714bd73e69f9f23a727999ea082
Gustavo Primolan <gustavoprimolan@yanksolutions.com.br>
2017-07-20 14:52:38 -0300

    Tirei um comentário

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -15,11 +15,11 @@ public class Main {
         File imagem = new File("imagensSeparadas/numeroConta.png");
         CharacterRecognizer cr = new CharacterRecognizer(imagem);
         String imagemBinaria = "imagensSeparadas/numeroContaBinario.png";
+        
         //NECESSÁRIO SEMPRE FAZER A BINARIAZAÇÃO DA IMAGEM
         cr.binarizaImagem(imagemBinaria, 155);
         
         File imgBinaria = new File(imagemBinaria);
-        //cr.imagemJusta(imgBinaria);
         String texto = cr.recognize(imgBinaria);
         
         System.out.println("Texto reconhecido: " + texto);
